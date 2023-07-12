b12e0e9d9f217f3f948ca1f4fc8c4e07c902895b
Jesper van Beemdelust <jespervanbeemdelust@hotmail.com>
2023-07-12 20:19:32 +0200

    Updated the folder structure for better reference

--- StrataScratch/01_Easy/Find how many times each artist appeared on the Spotify ranking list.sql
+++ StrataScratch/01_Easy/Find how many times each artist appeared on the Spotify ranking list.sql
@@ -1,8 +1,8 @@
 -- Find how many times each artist appeared on the Spotify ranking list
 
 SELECT 
-    artist, 
-    COUNT(artist) 
+        artist 
+    ,   COUNT(artist) AS count_artists
 FROM spotify_worldwide_daily_song_ranking
 GROUP BY artist
 ORDER BY 2 DESC;
