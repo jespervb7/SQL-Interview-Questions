e390e7983675b68cf4345bbf781ded7a884b4e3c
Jesper van Beemdelust <jespervanbeemdelust@hotmail.com>
2023-07-13 22:56:04 +0200

    Another interview question

---
+++ 01_StrataScratch/01_Easy/Most Lucrative Products.sql
@@ -0,0 +1,6 @@
+/* 
+Link to Stratascratch profile:          https://platform.stratascratch.com/user/Jespervb
+Link to Hackerrank profile:             https://www.hackerrank.com/jeppakanopolo?hr_r=1
+Link to current interview question:     
+Interview question title:               
+*/ 
