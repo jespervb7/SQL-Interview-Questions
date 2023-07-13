9b63e14d37987200f09ffe0982b58975d22af193
Jesper van Beemdelust <jespervanbeemdelust@hotmail.com>
2023-07-13 22:56:31 +0200

    Forgot to add the SQL portion

--- 01_StrataScratch/01_Easy/Most Lucrative Products.sql
+++ 01_StrataScratch/01_Easy/Most Lucrative Products.sql
@@ -1,6 +1,19 @@
 /* 
 Link to Stratascratch profile:          https://platform.stratascratch.com/user/Jespervb
 Link to Hackerrank profile:             https://www.hackerrank.com/jeppakanopolo?hr_r=1
-Link to current interview question:     
-Interview question title:               
+Link to current interview question:     https://platform.stratascratch.com/coding/2119-most-lucrative-products?code_type=5
+Interview question title:               Most Lucrative Products
 */ 
+
+WITH product_revenue AS (
+    SELECT
+            product_id
+        ,   sum(units_sold*cost_in_dollars) AS revenue
+    FROM online_orders
+    GROUP BY product_id
+)
+
+SELECT TOP (5)
+        *
+FROM product_revenue
+ORDER BY revenue DESC
