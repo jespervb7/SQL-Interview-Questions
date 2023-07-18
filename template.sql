a3071a1fe67a297b415bfbb1d0541de4f1aae2c5
Jesper van Beemdelust <jespervanbeemdelust@hotmail.com>
2023-07-18 20:23:51 +0200

    Fixed whitespace in the template

--- template.sql
+++ template.sql
@@ -3,4 +3,4 @@ Link to Stratascratch profile:          https://platform.stratascratch.com/user/
 Link to Hackerrank profile:             https://www.hackerrank.com/jeppakanopolo?hr_r=1
 Link to current interview question:     
 Interview question title:               
-*/ 
+*/
