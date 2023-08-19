21279f2b386f9287d5cb25b727ffe8023a4539b1
Jesper van Beemdelust <jespervanbeemdelust@hotmail.com>
2023-08-19 21:24:23 +0200

    Fully update the script

--- 04_datalemur/01_Easy/Page With No Likes [Facebook SQL Interview Question].sql
+++ 04_datalemur/01_Easy/Page With No Likes [Facebook SQL Interview Question].sql
@@ -1,6 +1,13 @@
 /* 
 Link to Stratascratch profile:          https://platform.stratascratch.com/user/Jespervb
 Link to Hackerrank profile:             https://www.hackerrank.com/jeppakanopolo?hr_r=1
-Link to current interview question:     
-Interview question title:               
+Link to current interview question:     https://datalemur.com/questions/sql-page-with-no-likes    
+Interview question title:               Page With No Likes [Facebook SQL Interview Question]    
 */
+
+SELECT
+      pa.page_id
+FROM pages pa
+LEFT JOIN page_likes li
+ON pa.page_id = li.page_id
+WHERE li.page_id is NULL;
